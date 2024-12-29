93f6452c52b106a73726fdeb3f6c43a5ae40b788
Vladimir Metodiev <vladimir.metodiev2907@gmail.com>
2024-12-29 22:34:03 +0200

    create register page

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,23 +1,22 @@
-import './Navbar.css'
-import { Link, useNavigate } from 'react-router-dom'
-import logo from '../../public/images/logo.png'
-import GuestUserLayout from '../NavbarOptions/GuestUser/GuestUserLayout'
-import LoggedUserLayout from '../NavbarOptions/LoggedInUser/LoggedUserLayout'
+import "./Navbar.css";
+import { Link, useNavigate } from "react-router-dom";
+import logo from "../../public/images/logo.png";
+import GuestUserLayout from "../NavbarOptions/GuestUser/GuestUserLayout";
+import LoggedUserLayout from "../NavbarOptions/LoggedInUser/LoggedUserLayout";
 export default function Navbar() {
-    const navigate = useNavigate()
+  const navigate = useNavigate();
 
-    return(
-        <div id='navbar'>
-            <div id='logo'>
-               <Link id='logo-wrapper'><img src={logo} to='/' alt='Logo' /></Link >
-            </div>
-            
-            <nav id='options'>
-                {
-                    false ? <GuestUserLayout/> : <LoggedUserLayout/>
-                }
-            </nav>
-        </div>
+  return (
+    <div id="navbar">
+      <div id="logo">
+        <Link id="logo-wrapper" to="/">
+          <img src={logo} to="/" alt="Logo" />
+        </Link>
+      </div>
 
-    )
+      <nav id="options">
+        {true ? <GuestUserLayout /> : <LoggedUserLayout />}
+      </nav>
+    </div>
+  );
 }
