b7b1ac1f746088788c761f4f7fc144b5102378f7
Vladimir Metodiev <vladimir.metodiev2907@gmail.com>
2025-02-04 15:45:31 +0200

    render data on other shops page

--- client/src/components/OtherShops/OtherShops.js
+++ client/src/components/OtherShops/OtherShops.js
@@ -1,6 +1,20 @@
-import ShopProfileSidebar from "../ShopProfileSidebar/ShopProfileSidebar";
 import "./OtherShops.css";
+import ShopProfileSidebar from "../ShopProfileSidebar/ShopProfileSidebar";
+import { useState, useEffect } from "react";
+import { getAllShopData } from "../../services/createShop";
+
 export default function OtherShops() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchedShopData = async () => {
+      const response = await getAllShopData();
+
+      setData(response);
+    };
+    fetchedShopData();
+  }, []);
+
   return (
     <div id="shop-profile-wrapper">
       <ShopProfileSidebar />
@@ -23,54 +37,51 @@ export default function OtherShops() {
             </div>
           </div>
           <div id="shops-container">
-            <div className="shop-container">
-              <div className="asd">
-                <div className="shop-initial-info">
-                  <div className="shop-initial-image-container">
-                    <img
-                      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6NBYcpeDOdLTlpPJEYPFI79VXakDA4yKtBw&s"
-                      alt=""
-                    />
+            {data.map((shop) => {
+              return (
+                <div className="shop-container">
+                  <div className="asd">
+                    <div className="shop-initial-info">
+                      <div className="shop-initial-image-container">
+                        <img src={shop.logo} alt={shop.name} />
+                      </div>
+                      <div className="shop-initial-name-conatiner">
+                        <p>{shop.name}</p>
+                      </div>
+                    </div>
+                    <div className="shop-creation-info">
+                      <div>
+                        <p>Created On:</p>
+                      </div>
+                      <div className="shop-creation-date">
+                        <p>12/01/2025.</p>
+                      </div>
+                    </div>
                   </div>
-                  <div className="shop-initial-name-conatiner">
-                    <p>Nike</p>
-                  </div>
-                </div>
-                <div className="shop-creation-info">
                   <div>
-                    <p>Created On:</p>
-                  </div>
-                  <div className="shop-creation-date">
-                    <p>12/01/2025.</p>
-                  </div>
-                </div>
-              </div>
-              <div>
-                <div className="shop-listings-info">
-                  <div>
-                    <div>Listings:</div>
-                  </div>
-                  <div className="shop-listing-info-count">
-                    <div>2</div>
-                  </div>
-                </div>
-                <div className="shop-categories-info">
-                  <div>
-                    <p>Categories:</p>
-                  </div>
-                  <div className="shop-categories-info-names">
-                    <p>Shoes, Bags</p>
+                    <div className="shop-listings-info">
+                      <div>
+                        <div>Listings:</div>
+                      </div>
+                      <div className="shop-listing-info-count">
+                        <div>{shop.listings.length}</div>
+                      </div>
+                    </div>
+                    <div className="shop-categories-info">
+                      <div>
+                        <p>Categories:</p>
+                      </div>
+                      <div className="shop-categories-info-names">
+                        <p>{shop.categories.join(", ")}</p>
+                      </div>
+                    </div>
+                    <div className="shop-button">
+                      <button>Visit Shop</button>
+                    </div>
                   </div>
                 </div>
-                <div className="shop-button">
-                  <button>Visit Shop</button>
-                </div>
-              </div>
-            </div>
-            <div className="shop-container"></div>
-            <div className="shop-container"></div>
-            <div className="shop-container"></div>
-            <div className="shop-container"></div>
+              );
+            })}
           </div>
           <div id="shops-paging">TO DO paging</div>
         </div>
--- client/src/services/createShop.js
+++ client/src/services/createShop.js
@@ -28,3 +28,37 @@ export const getShopData = async (id) => {
     console.log(error);
   }
 };
+
+export const createListing = async (listingData, user) => {
+  try {
+    const response = await fetch(
+      `http://localhost:3000/create-listing?name=${user}`,
+      {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(listingData),
+      }
+    );
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.log("This is the error: ");
+    console.log(error);
+  }
+};
+
+export const getAllShopData = async (id) => {
+  try {
+    const response = await fetch(`http://localhost:3000/other-shops`);
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.log("This is the error: ");
+    console.log(error);
+  }
+};
