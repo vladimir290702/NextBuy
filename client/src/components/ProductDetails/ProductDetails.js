fbc964d07a0490015c44ac7ce58d24252aa7f331
Vladimir Metodiev <vladimir.metodiev2907@gmail.com>
2025-02-06 15:46:08 +0200

    render product details with proper information

--- client/src/components/ProductDetails/ProductDetails.js
+++ client/src/components/ProductDetails/ProductDetails.js
@@ -1,11 +1,29 @@
 import "./ProductDetails.css";
 import { FaHeart, FaRegHeart } from "react-icons/fa";
 import { TbTruckDelivery, TbPackageImport } from "react-icons/tb";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ProductCategories from "../ProductCategories/ProductCategories";
+import { useLocation } from "react-router-dom";
+import { getListing } from "../../services/createShop";
 
 export default function ProductDetails() {
+  const location = useLocation();
   const [selectedFavourite, setSelectedFavourite] = useState(false);
+  const [listingData, setListingData] = useState([]);
+  const id = location.state.id;
+
+  console.log(listingData);
+
+  useEffect(() => {
+    const fetchedShopData = async () => {
+      const response = await getListing(id);
+
+      console.log(response);
+
+      setListingData(response);
+    };
+    fetchedShopData();
+  }, []);
 
   const handleFavouriteProduct = (e) => {
     e.preventDefault();
@@ -18,54 +36,37 @@ export default function ProductDetails() {
       <ProductCategories />
       <div id="product-details-wrapper">
         <div id="product-details-images-container">
-          <div className="product-details-image">
-            <img
-              src="https://static.nike.com/a/images/t_PDP_936_v1/f_auto,q_auto:eco/6ca23f73-976a-47c0-86f9-a6a7ab527130/AIR+MAX+PLUS+DRIFT.png"
-              alt=""
-            />
-          </div>
-          <div className="product-details-image">
-            <img
-              src="https://static.nike.com/a/images/t_PDP_936_v1/f_auto,q_auto:eco/6ca23f73-976a-47c0-86f9-a6a7ab527130/AIR+MAX+PLUS+DRIFT.png"
-              alt=""
-            />
-          </div>
-          <div className="product-details-image">
-            <img
-              src="https://static.nike.com/a/images/t_PDP_936_v1/f_auto,q_auto:eco/6ca23f73-976a-47c0-86f9-a6a7ab527130/AIR+MAX+PLUS+DRIFT.png"
-              alt=""
-            />
-          </div>
-          <div className="product-details-image">
-            <img
-              src="https://static.nike.com/a/images/t_PDP_936_v1/f_auto,q_auto:eco/6ca23f73-976a-47c0-86f9-a6a7ab527130/AIR+MAX+PLUS+DRIFT.png"
-              alt=""
-            />
-          </div>
+          {listingData?.product?.images.map((product) => {
+            return (
+              <div className="product-details-image">
+                <img src={product} alt={product} />
+              </div>
+            );
+          })}
         </div>
         <div id="product-details-information-container">
           <div id="routes">
             <p>Man - Shoes - Sport shoes</p>
           </div>
           <div id="product-name">
-            <p>Nike Air Force</p>
+            <p>
+              {listingData.product.productName} {listingData.product.model}
+            </p>
           </div>
           <div id="product-price">
-            <p>$129.99</p>
+            <p>${listingData.product.price}</p>
           </div>
           <div>
-            <p>Color: White</p>
+            <p>Color: {listingData.product.color}</p>
           </div>
           <div>
             <p>Sizes:</p>
           </div>
+
           <div id="product-details-sizes-container">
-            <div className="product-size">39</div>
-            <div className="product-size">40</div>
-            <div className="product-size">41</div>
-            <div className="product-size">42</div>
-            <div className="product-size">43</div>
-            <div className="product-size">44</div>
+            {listingData.product.sizes.map((size) => {
+              return <div className="product-size">{size}</div>;
+            })}
           </div>
           <div id="product-details-buttons-container">
             <div id="add-to-bag-button">
@@ -101,14 +102,7 @@ export default function ProductDetails() {
             </div>
           </div>
           <div id="details-container">
-            <p>
-              This T-shirt is crafted from 100% regenerative cotton, excluding
-              trims. The cotton is grown using farming methods that seek to
-              improve soil health, watersheds and biodiversity. • cotton jersey
-              • regular fit • crew neckline • short sleeves • Calvin Klein
-              monogram embroidery on the chest composition and care: 100% cotton
-              machine wash tumble dry low
-            </p>
+            <p>{listingData.product.description} </p>
           </div>
         </div>
       </div>
