fe3a94ca958b405717f678d93d5d93a9733cd887
Vladimir Metodiev <vladimir.metodiev2907@gmail.com>
2025-02-06 16:17:57 +0200

    fix details page

--- client/src/components/ProductDetails/ProductDetails.js
+++ client/src/components/ProductDetails/ProductDetails.js
@@ -9,20 +9,23 @@ import { getListing } from "../../services/createShop";
 export default function ProductDetails() {
   const location = useLocation();
   const [selectedFavourite, setSelectedFavourite] = useState(false);
-  const [listingData, setListingData] = useState([]);
+  const [listingData, setListingData] = useState(null);
+  const [loading, setLoading] = useState(true);
   const id = location.state.id;
 
-  console.log(listingData);
-
   useEffect(() => {
-    const fetchedShopData = async () => {
-      const response = await getListing(id);
+    try {
+      const fetchedShopData = async () => {
+        const response = await getListing(id);
 
-      console.log(response);
-
-      setListingData(response);
-    };
-    fetchedShopData();
+        setListingData(response);
+      };
+      fetchedShopData();
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
   }, []);
 
   const handleFavouriteProduct = (e) => {
@@ -33,79 +36,86 @@ export default function ProductDetails() {
 
   return (
     <>
-      <ProductCategories />
-      <div id="product-details-wrapper">
-        <div id="product-details-images-container">
-          {listingData?.product?.images.map((product) => {
-            return (
-              <div className="product-details-image">
-                <img src={product} alt={product} />
+      {loading ? null : (
+        <>
+          <ProductCategories />
+          <div id="product-details-wrapper">
+            <div id="product-details-images-container">
+              {listingData?.product.images.map((product) => {
+                return (
+                  <div className="product-details-image">
+                    <img src={product} alt={product} />
+                  </div>
+                );
+              })}
+            </div>
+            <div id="product-details-information-container">
+              <div id="routes">
+                <p>Man - Shoes - {listingData?.product.category}</p>
               </div>
-            );
-          })}
-        </div>
-        <div id="product-details-information-container">
-          <div id="routes">
-            <p>Man - Shoes - Sport shoes</p>
-          </div>
-          <div id="product-name">
-            <p>
-              {listingData.product.productName} {listingData.product.model}
-            </p>
-          </div>
-          <div id="product-price">
-            <p>${listingData.product.price}</p>
-          </div>
-          <div>
-            <p>Color: {listingData.product.color}</p>
-          </div>
-          <div>
-            <p>Sizes:</p>
-          </div>
+              <div id="product-name">
+                <p>
+                  {listingData?.product.productName}{" "}
+                  {listingData?.product.model}
+                </p>
+              </div>
+              <div id="product-price">
+                <p>${listingData?.product?.price}</p>
+              </div>
+              <div>
+                <p>Color: {listingData?.product?.color}</p>
+              </div>
+              <div>
+                <p>Sizes:</p>
+              </div>
 
-          <div id="product-details-sizes-container">
-            {listingData.product.sizes.map((size) => {
-              return <div className="product-size">{size}</div>;
-            })}
-          </div>
-          <div id="product-details-buttons-container">
-            <div id="add-to-bag-button">
-              <button>Add to Bag</button>
-            </div>
-            <div
-              id="favourite-button"
-              onClick={(e) => handleFavouriteProduct(e)}
-            >
-              {selectedFavourite ? (
-                <FaHeart id="favourite-pressed" />
-              ) : (
-                <FaRegHeart />
-              )}
-            </div>
-          </div>
-          <div id="delivery-container">
-            <div className="delivery-info">
-              <div>
-                <TbTruckDelivery className="delivery-icon" />
+              <div id="product-details-sizes-container">
+                {listingData?.product.sizes.map((size) => {
+                  return <div className="product-size">{size}</div>;
+                })}
               </div>
-              <div>
-                <p>Free delivery on all orders over $75</p>
+              <div id="product-details-buttons-container">
+                <div id="add-to-bag-button">
+                  <button>Add to Bag</button>
+                </div>
+                <div
+                  id="favourite-button"
+                  onClick={(e) => handleFavouriteProduct(e)}
+                >
+                  {selectedFavourite ? (
+                    <FaHeart id="favourite-pressed" />
+                  ) : (
+                    <FaRegHeart />
+                  )}
+                </div>
               </div>
-            </div>
-            <div className="delivery-info">
-              <div>
-                <TbPackageImport className="delivery-icon" />
+              <div id="delivery-container">
+                <div className="delivery-info">
+                  <div>
+                    <TbTruckDelivery className="delivery-icon" />
+                  </div>
+                  <div>
+                    <p>Free delivery on all orders over $75</p>
+                  </div>
+                </div>
+                <div className="delivery-info">
+                  <div>
+                    <TbPackageImport className="delivery-icon" />
+                  </div>
+                  <div>
+                    <p>
+                      60-day returns. For returns, a fee of 4,95 EUR applies.
+                    </p>
+                  </div>
+                </div>
               </div>
-              <div>
-                <p>60-day returns. For returns, a fee of 4,95 EUR applies.</p>
+              <div id="details-container">
+                <p>{listingData?.product.description} </p>
               </div>
             </div>
           </div>
-          <div id="details-container">
-            <p>{listingData.product.description} </p>
-          </div>
-        </div>
-      </div>
+        </>
+      )}
     </>
   );
 }
