e7133cac71a4cad7625595a3766f050acd200065
Vladimir Metodiev <vladimir.metodiev2907@gmail.com>
2025-02-24 20:31:16 +0200

    change the design of review order page

--- client/src/components/ReviewOrder/ReviewOrder.js
+++ client/src/components/ReviewOrder/ReviewOrder.js
@@ -1,41 +1,44 @@
 import "./ReviewOrder.css";
+import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import ShopProfileSidebar from "../ShopProfileSidebar/ShopProfileSidebar";
-
+import { FaCheck } from "react-icons/fa";
 export default function ReviewOrder({ item }) {
   const { state } = useLocation();
+  const [animate, setAnimate] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => setAnimate(true), 200); // Delay start for smoother effect
+  }, []);
+
+  const statuses = ["pre-transit", "in-transit", "delivered"];
+  const statusIndex = statuses.indexOf("in-transit");
 
   return (
     <div id="review-order-container">
       <ShopProfileSidebar />
       <div id="review-order-wrapper">
-        <div id="review-order-title-conatainer">
-          <h2>Order #{state.order.trackingNumber}</h2>
-        </div>
-        <div id="review-order-images-container">
-          {state.order.orderedProducts.map((product) => {
-            return (
-              <div className="review-order-image-container" key={product.model}>
-                <div>
-                  <img src={product.images[0]} alt={product.productName} />
-                </div>
-                <div>
-                  <p>
-                    {product.productName} {product.model}
-                  </p>
-                </div>
-              </div>
-            );
-          })}
-        </div>
-        <div id="review-order-subtotal-container">
-          <div>
-            <h3>Subtotal: ${state.order.subtotal}</h3>
+        <div className={`review-order-container ${animate ? "slide-in" : ""}`}>
+          <div className={`circle ${statusIndex >= 0 ? "active" : ""}`}>
+            <FaCheck />
+          </div>
+          <div
+            className={`line ${animate && statusIndex >= 1 ? "fill" : ""}`}
+          ></div>
+          <div className={`circle ${statusIndex >= 1 ? "active" : ""}`}>
+            <FaCheck />
+          </div>
+          <div
+            className={`line ${animate && statusIndex >= 2 ? "fill" : ""}`}
+          ></div>
+          <div className={`circle ${statusIndex >= 2 ? "active" : ""}`}>
+            <FaCheck />
           </div>
         </div>
-        <div id="review-order-shipping-details">
+
+        <div id="review-order-product-information-and-shipping-wrapper">
           <div id="review-order-shipping-information">
-            <p>Ship to:</p>
+            <h2>Ship to:</h2>
             <div id="review-order-address-information">
               <p>
                 {state.order.firstName} {state.order.lastName}
@@ -46,16 +49,59 @@ export default function ReviewOrder({ item }) {
               </p>
             </div>
           </div>
-          <div id="review-order-status">
-            <p>Change status:</p>
-
-            <div id="review-order-status-dropdown">
-              <select name="" id="">
-                <option value="">Pre-Transit</option>
-                <option value="">In Transit</option>
-                <option value="">Completed</option>
-              </select>
+          <div id="review-order-shipping-details">
+            <div id="review-order-status">
+              <p>Change status:</p>
+              <div id="review-order-status-dropdown">
+                <select name="" id="">
+                  <option value="">Pre-Transit</option>
+                  <option value="">In Transit</option>
+                  <option value="">Completed</option>
+                </select>
+              </div>
             </div>
+          </div>
+        </div>
+        <div id="review-order-images-container">
+          <div>
+            <h3>Item(s):</h3>
+            {state.order.orderedProducts.map((product) => {
+              return (
+                <div
+                  className="review-order-image-container"
+                  key={product.model}
+                >
+                  <div>
+                    <img src={product.images[0]} alt={product.productName} />
+                  </div>
+                  <div className="review-order-product-description">
+                    <h1 className="review-order-product-model">
+                      {product.productName} {product.model}
+                    </h1>
+                    <p>Category: {product.category}</p>
+                    <p>Price: ${product.price}</p>
+                    <p>Qantity: {product.quantity}</p>
+                    <p>Total: ${product.price * product.quantity}</p>
+                    <p>Arrives: Fri, 19/04 - Mon, 21/04</p>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div id="review-order-contact-custommer-container">
+          <div id="review-order-contact-custommer">
+            <h2>Contact custommer:</h2>
+
+            <p>Email: {state.order.user}</p>
+            <p>
+              Name: {state.order.firstName} {state.order.lastName}
+            </p>
+          </div>
+        </div>
+        <div id="review-order-subtotal-container">
+          <div>
+            <h3>TO DO: Payment + Billing Address</h3>
           </div>
         </div>
       </div>
